4bd46f58657a41583632fd3a76782ee866e864af
j-w-k <kim.whyijun@gmail.com>
2021-08-07 22:48:35 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new TextEditor();
+        //LaunchPage launchPage = new LaunchPage();
+
+        /*
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are "+height+" cm tall");
+        */
+        /*
+        String[] cars = new String[3];
+
+        cars[0] = "Camaro";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
+
+        for(int i=0; i<cars.length; i++){
+            System.out.println(cars[i]);
+        }
+        */
+    }
+}
